3d3ae1ec2d6fe5edfd5c6d51e05b0e4a3018db80
LeoZion <leozionsuporte@gmail.com>
2022-07-19 19:47:17 -0300

    add first pages

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, illum itaque tenetur beatae ea, unde accusantium omnis enim reiciendis ipsum nemo exercitationem cum doloremque tempore ratione vero accusamus eius aperiam?</p>
+    </div>
+  )
+}
