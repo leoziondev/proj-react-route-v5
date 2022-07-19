ce5df9617f794c9f08143928187084d5a239ccd3
LeoZion <leozionsuporte@gmail.com>
2022-07-19 20:31:53 -0300

    fetching data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { useFetch } from "../hooks/useFetch"
+
+import './Home.css'
+
 export function HomePage() {
+  const { data:articles, isPending, error } = useFetch('http://localhost:3000/articles')
+
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, illum itaque tenetur beatae ea, unde accusantium omnis enim reiciendis ipsum nemo exercitationem cum doloremque tempore ratione vero accusamus eius aperiam?</p>
+    <div className="home">
+      <h1>Articles</h1>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+
+      {articles && articles.map((article) => (
+        <article key={article.id} className="card">
+          <h2>{article.title}</h2>
+          <p><strong>Author</strong>: {article.author}</p>
+        </article>
+      ))}
     </div>
   )
 }
