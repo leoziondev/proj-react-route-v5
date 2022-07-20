30ba5f2f2475d6114d4a6336bd50a506779908e2
LeoZion <leozionsuporte@gmail.com>
2022-07-19 23:26:40 -0300

    query parameters

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom"
+
 export function ContactPage() {
+  const queryString = useLocation().search
+  console.log(queryString);
+
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+  // ?name=jhon
+
   return (
     <div>
-      <h1>Contact</h1>
+      <h1>Hey {name} Contact us here..</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, illum itaque tenetur beatae ea, unde accusantium omnis enim reiciendis ipsum nemo exercitationem cum doloremque tempore ratione vero accusamus eius aperiam?</p>
     </div>
   )
